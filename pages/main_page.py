4a6f68613bbd941f8feb942ac8705b33733e093c
Marina Glinskaya <sangriyam@yandex.ru>
2026-08-19 13:09:58 +1100

    Move main page locators to separate file

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,38 +1,10 @@
 from selenium.webdriver.common.by import By
 
 from pages.base_page import BasePage
+from pages.main_page_locators import MainPageLocators
 
 
 class MainPage(BasePage):
-    TOP_ORDER_BUTTON = (
-        By.XPATH,
-        "//div[contains(@class, 'Header_Nav')]/button[text()='Заказать']"
-    )
-
-    BOTTOM_ORDER_BUTTON = (
-        By.XPATH,
-        "//div[contains(@class, 'Home_FinishButton')]/button[text()='Заказать']"
-    )
-
-    COOKIE_BUTTON = (
-        By.ID,
-        "rcc-confirm-button"
-    )
-
-    FAQ_SECTION = (
-        By.XPATH,
-        "//div[text()='Вопросы о важном']"
-    )
-
-    SCOOTER_LOGO = (
-        By.XPATH,
-        "//img[@alt='Scooter']"
-    )
-
-    YANDEX_LOGO = (
-        By.XPATH,
-        "//img[@alt='Yandex']"
-    )
 
     @staticmethod
     def question_locator(index):
@@ -52,18 +24,18 @@ class MainPage(BasePage):
         self.open(url)
 
     def accept_cookies(self):
-        self.click(self.COOKIE_BUTTON)
+        self.click(MainPageLocators.COOKIE_BUTTON)
         
 
     def click_top_order_button(self):
-        self.click(self.TOP_ORDER_BUTTON)
+        self.click(MainPageLocators.TOP_ORDER_BUTTON)
 
     def click_bottom_order_button(self):
-        self.scroll_to_element(self.BOTTOM_ORDER_BUTTON)
-        self.click(self.BOTTOM_ORDER_BUTTON)
+        self.scroll_to_element(MainPageLocators.BOTTOM_ORDER_BUTTON)
+        self.click(MainPageLocators.BOTTOM_ORDER_BUTTON)
 
     def scroll_to_faq(self):
-        self.scroll_to_element(self.FAQ_SECTION)
+        self.scroll_to_element(MainPageLocators.FAQ_SECTION)
 
     def click_question(self, index):
         locator = self.question_locator(index)
@@ -76,7 +48,7 @@ class MainPage(BasePage):
         )
 
     def click_scooter_logo(self):
-        self.click(self.SCOOTER_LOGO)
+        self.click(MainPageLocators.SCOOTER_LOGO)
 
     def click_yandex_logo(self):
-        self.click(self.YANDEX_LOGO)
+        self.click(MainPageLocators.YANDEX_LOGO)
